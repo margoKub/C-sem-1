bb7db0fd4c1364a77330035cb78417e9ab1a9057
MargaritaKub <lora313@yandex.ru>
2022-12-26 11:57:52 +0300

    task003

---
+++ task003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = -n;
+while (k < n + 1)
+{
+    Console.Write($"{k} ");
+    k++;
+}
+Console.WriteLine();
