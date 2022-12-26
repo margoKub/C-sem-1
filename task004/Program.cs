8af1e0582eaed740f52bb2a48b5d5e5230f2f7db
MargaritaKub <lora313@yandex.ru>
2022-12-26 12:30:02 +0300

    task004

---
+++ task004/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите целое трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int last_digit = number % 10;
+Console.WriteLine($"The last digit of the number is {last_digit}");
+
+
